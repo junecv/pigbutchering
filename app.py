9c93c9fd413cd55387dcb105da1c4fadb83117e3
June <tumbledrycold@gmail.com>
2022-08-21 19:50:09 -0700

    update app with text vectorizer, model

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import streamlit as st
+import pickle as pkl
+import tensorflow as tf
+from tensorflow.keras import layers
 
+# retrieve text vectorization layer 
+tv_spec = pkl.load(Open('model/tv_layer.pkl', 'rb'))
+text_vectorizer = layers.TextVectorization.from_config(tv_spec['config'])
+text_vectorizer.set_weights(tv_spec['weights'])
+
+# load model
+model = tf.keras.models.load_model('model/dnn_model')
+
+# get input
 text = st.text_input('check if you\'re the \U0001F437', 'Hi Disky, how is your business doing?')
 
 if text:
-    out = 'business' in text
-    st.write('text include keyword business', out)
+    text_vector = text_vectorizer(text)
+    out = model.predict(text_vector)[0][0]
+    st.write('Your \U0001F437 score is', out)
+    if out > 0.5:
+        st.write('I smell \U0001F953')
+    else: st.write('Well, think twice.')
