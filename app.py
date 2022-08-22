babf3896a3938012f6a4b772f1ae50442d39db78
June <tumbledrycold@gmail.com>
2022-08-21 19:53:44 -0700

    fixed bug

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 # retrieve text vectorization layer 
-tv_spec = pkl.load(Open('model/tv_layer.pkl', 'rb'))
+tv_spec = pkl.load(Open('saved_model/tv_layer.pkl', 'rb'))
 text_vectorizer = layers.TextVectorization.from_config(tv_spec['config'])
 text_vectorizer.set_weights(tv_spec['weights'])
 
 # load model
-model = tf.keras.models.load_model('model/dnn_model')
+model = tf.keras.models.load_model('saved_model/dnn_model')
 
 # get input
 text = st.text_input('check if you\'re the \U0001F437', 'Hi Disky, how is your business doing?')
