b8b4e3afd3fabf1f652c72eb3c5a6643d041b7f5
June <tumbledrycold@gmail.com>
2022-08-21 21:34:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ text = st.text_input('check if you\'re the \U0001F437', 'Hi Disky, how is your b
 
 if text:
     text_vector = text_vectorizer(text)
-    out = model.predict(text_vector)[0][0]
+    out = model.predict(text_vector)
     st.json(out)
     # st.write('Your \U0001F437 score is', out)
     # if out > 0.5:
