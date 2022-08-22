2fd9e5ec3e652e078b2482583b7a8b601c1a2ee7
June <tumbledrycold@gmail.com>
2022-08-21 21:48:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ if text:
     text = [text]
     text_vector = text_vectorizer(text)
     out = model.predict(text_vector)[0][0]
-    st.write(out)
-    # st.write('Your \U0001F437 score is', out)
-    # if out > 0.5:
-    #     st.write('I smell \U0001F953')
-    # else: st.write('Well, think twice.')
+    st.write('Your \U0001F437 score is', out)
+    if out > 0.5:
+        st.write('I smell \U0001F953')
+    else: st.write('Well, think twice, think twice.')
